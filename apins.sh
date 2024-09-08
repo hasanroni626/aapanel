bb3cd2af0fb00325469614ae5ed95ef615f14a46
hasanroni626 <127964109+hasanroni626@users.noreply.github.com>
2024-09-08 07:52:21 +0600

    Update apins.sh 1.0.1

--- apins.sh
+++ apins.sh
@@ -1,2 +1,3 @@
 echo "Installing aapanel - Mendex Junior | SUMITBD"
+echo "Use case: Training Purposes"
 wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh 93684c35
